567dfc01ac2a58f037ebe031b8fcba3add1be136
ltbob <cpt.bob@gmail.com>
2024-08-21 14:25:35 -0600

    Start of the random quote app with basic look

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Here's a random quote to brighten your day</h1>
       </header>
+      <div className="main">
+        <div id="wrapper" className="App-container">
+          <div id="quote-box" className="quote-box">
+            <div class="quote-text">
+              <span id="text"></span>
+            </div>
+            <div class="quote-author">
+              <span id="author"></span>
+            </div>
+            <div className="share-button">
+              <a class="button"></a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
