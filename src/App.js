e759cdbc720528e945e9a6c767e1b76948592440
ltbob <cpt.bob@gmail.com>
2024-08-21 15:29:34 -0600

    Added font-awesome icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 //import logo from "./logo.svg";
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faQuoteLeft, faQuoteRight } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Here's a random quote to brighten your day</h1>
+        <h1>Here is a random quote to brighten your day</h1>
       </header>
       <div className="main">
         <div id="wrapper" className="App-container">
           <div id="quote-box" className="quote-box">
             <div class="quote-text">
-              <span id="text"></span>
+              <FontAwesomeIcon icon={faQuoteLeft} />
+              <span id="text">Well done is better than well said</span>
+              <FontAwesomeIcon icon={faQuoteRight} />
             </div>
             <div class="quote-author">
-              <span id="author"></span>
+              <span id="author">- Benjamin Franklin</span>
             </div>
-            <div className="share-button">
-              <a class="button"></a>
+            <div className="buttons">
+              <div className="share-button">
+                <a class="button" href="https://twitter.com/intent/tweet">
+                  <FontAwesomeIcon icon={faSquareXTwitter} />
+                </a>
+                <button class="button" id="new-quote">
+                  New quote
+                </button>
+              </div>
             </div>
           </div>
         </div>
